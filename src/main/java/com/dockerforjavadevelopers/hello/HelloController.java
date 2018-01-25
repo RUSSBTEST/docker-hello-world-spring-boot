645170559355563adac3bae24665c9eb2a810495
Hieu Pham <hieu.pham@ontario.ca>
2018-01-24 23:27:19 -0500

    added bye message

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         
-        String message = "Hello World!\n" + new java.util.Date().toString();
+        String message = "Hello World!\n" + new java.util.Date().toString() + "\nOk bye!\n";
         return message;
     }
     
